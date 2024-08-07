06349ead0868188fb714e32920780be137c41581
Jad <jadmakke7@gmail.com>
2024-08-07 11:00:01 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import pyautogui as agui
+agui.hotkey('winleft', 'd')
+agui.hotkey('winleft', 's')
+agui.typewrite("Chrome")
+agui.press("enter")
+agui.sleep(3)
+agui.click(350, 300)
+agui.click(351, 301)
+agui.sleep(3)
+agui.typewrite("bestfig.xyz")
+agui.press("enter")
